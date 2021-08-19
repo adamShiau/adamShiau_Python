fda467a39174e4edf86a61c98c64d991aaf258a5
adam1025 <69100634+adamShiau@users.noreply.github.com>
2021-08-20 00:02:50 +0800

    update

--- EarthQ/tt.py
+++ EarthQ/tt.py
@@ -8,56 +8,92 @@ dirname = os.path.dirname(__file__) #get path of current file
 dataPath = os.path.join(dirname, 'data') #enter the directory name to save data
 # print(dataPath)
 
-def autoCreateDir(dataPath, dirName):
+''' 
+autoCreateDir: 檢查給定路徑資料夾是否存在，若無則建立資料夾，若有則不做任何事
+輸入:
+dataPath : 資料夾之絕對路徑
+dirName : 資料夾名稱
+busy: 若為1則不做任何事
+輸出: 
+status: 	0.=不存在資料夾，建立資料夾成功
+			1.=已存在資料夾，不建立新資料夾
+			2.=目前狀態為busy，不做任何事
+filepath: 輸出資料夾絕對路徑
+
+'''
+def autoCreateDir(dataPath, dirName, busy):
 	filepath = os.path.join(dataPath, str(dirName))
-	folder_exist = os.path.exists(filepath)
-	if not folder_exist:
-		print('\ncreate folder: ', filepath)
-		os.mkdir(filepath)
-		success = 1
+	if not busy:
+		folder_exist = os.path.exists(filepath)
+		if not folder_exist:
+			print('\ncreate folder: ', filepath)
+			os.mkdir(filepath)
+			status = 0
+		else:
+			print('\nfolder exist!')
+			status = 1
 	else:
-		print('\nfolder exist!')
-		success = 0
-	return success, filepath
-	
-def open_and_save_data(cnt, dataNum, filePath, data1, data2, idx):
-	if(cnt==dataNum): 
+		status = 2
+	return status, filepath
+''' 
+open_and_save_data: 輸入計數器與最大資料數量，
+'''
+def open_and_save_data(data_ptr, max_dataNum, rst_n, filePath, idx, data1, data2):
+	print('in_data_ptr: ', data_ptr)
+	# print('in_ busy: ', busy)
+	print('in_ idx: ', idx)
+	if(data_ptr == max_dataNum): 
 		f=open(os.path.join(filePath, str(idx)) +'.txt', 'w')
 		f.writelines('#' + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + '\n')
-		status = 1
-	elif(cnt==1):
+		np.savetxt(f, np.vstack([data1, data2]).T, fmt='%d, %d')
+		busy = 1
+		status = 0
+		f.close
+	elif(data_ptr == 1 or rst_n == 0):
+		np.savetxt(f, np.vstack([data1, data2]).T, fmt='%d, %d')
 		f.writelines('#' + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + '\n')
 		f.close
-		status = 0
+		busy = 0
+		status = 1
 	else:
 		np.savetxt(f, np.vstack([data1, data2]).T, fmt='%d, %d')
+		busy = 1
 		status = 2
-	return status
+	return status, busy
 	
 if __name__ == '__main__':
+	#initialization
+	max_dataNum = 10
+	g_busy = 0
+	g_data_ptr = max_dataNum
+	rst_n = 1 #self.run_flag
+	g_idx = 1
 	print('current path: ', dirname)
-	flag1, filepath1 = autoCreateDir(dataPath,  datetime.datetime.now().year)
-	flag2, filepath2 = autoCreateDir(filepath1, datetime.datetime.now().month)
-	print('flag1: ', flag1)
-	print('filepath1: ', filepath1)
-	print('flag2: ', flag2)
-	print('filepath2: ', filepath2)
-	
-	# f=open(os.path.join(filepath2, str(0)) + '.txt', 'a')
-	# np.savetxt(f, np.array([456]), fmt='%d')
-	# f.close
-	
-	cnt = 100
-	dataNum = 100
-	data1 = 11
-	data2 = 12
-	idx = 0
-	
-	for i in range(1,500):
-		status = open_and_save_data(cnt, dataNum, filepath2, data1, data2, idx)
-		if(cnt == 1):
-			cnt = 100
-		else:
-			cnt = cnt - 1
+	for i in range(1, 50):
+		
+		status, filepath1 = autoCreateDir(dataPath,  datetime.datetime.now().year,  g_busy)
+		status, filepath2 = autoCreateDir(filepath1, datetime.datetime.now().month, g_busy)
+		print('i= ', i, end='\t')
+		print('g_busy= ', g_busy, end='\t')
+		print('status= ', status, end='\t')
+		print('g_data_ptr= ', g_data_ptr, end='\t')
+		print('rst_n= ', rst_n, end='\t')
+		print('g_idx= ', g_idx)
+		aa, g_busy = open_and_save_data(g_data_ptr, max_dataNum, rst_n, filepath2, g_idx, 456, 123)
 		if(status == 0):
-			idx = idx + 1
+			g_idx = 0 #如果建立新資料夾，重製檔案名稱_idx
+		if(rst_n):
+			g_idx = g_idx + 1
+		if(g_data_ptr == 1):#資料數量指標=1時代表關閉檔案了，此時重制至max_dataNum
+			g_data_ptr = max_dataNum
+		g_data_ptr = g_data_ptr - 1
+		if(i==50):
+			rst_n = 0
+		if(i==60):
+			rst_n = 1
+	
+	# print('status1: ', status1)
+	# print('filepath1: ', filepath1)
+	# print('status2: ', status2)
+	# print('filepath2: ', filepath2)
+	
--- MV/MV.py
+++ MV/MV.py
@@ -16,13 +16,15 @@ def get_sma_cutoff(N, **kwargs):
     return newton(func, omega_0, deriv, **kwargs)
 
 # Simple moving average design parameters
-f_s = 100
-N = 64
+f_s = 100e6
+N = 128
 
 # Find the cut-off frequency of the SMA
 w_c = get_sma_cutoff(N)
 f_c = w_c * f_s / (2 * pi)
-print('f_c: ', f_c)
+print('sampling rate(MHz): ',f_s/1e6 );
+print('MV points: ',N );
+print('cut off freq(KHz) : ', f_c/1e3, end='\n\n')
 
 # SMA coefficients
 b = np.ones(N)
@@ -30,30 +32,41 @@ a = np.array([N] + [0]*(N-1))
 
 # Calculate the frequency response
 w, h = freqz(b, a, worN=4096)
-w *= f_s / (2 * pi)                      # Convert from rad/sample to Hz
+w *= f_s / (2 * pi)                    # Convert from rad/sample to Hz
+
+# f_marker1 = 24.4e3
+f_marker2 = 389e3
+# f_marker3 = 775e3
 
 # Plot the amplitude response
 plt.subplot(2, 1, 1)
 plt.suptitle('Bode Plot')
 plt.plot(w, 20 * np.log10(abs(h)))       # Convert modulus to dB
 plt.ylabel('Magnitude [dB]')
-plt.xlim(0, f_s / 2)
-plt.ylim(-60, 10)
-plt.axvline(f_c, color='red')
-plt.axhline(-3.01, linewidth=0.8, color='black', linestyle=':')
+plt.xlim(0, f_s / 2 /50)
+plt.ylim(-30, 1)
+# plt.axvline(f_c, color='red')
+# plt.axvline(f_marker1, color='blue')
+plt.axvline(f_marker2, color='blue')
+# plt.axvline(f_marker3, color='blue')
+# plt.axhline(-3.01, linewidth=0.8, color='black', linestyle=':')
+plt.grid()
 
-''' 
 # Plot the phase response
 plt.subplot(2, 1, 2)
 plt.plot(w, 180 * np.angle(h) / pi)      # Convert argument to degrees
 plt.xlabel('Frequency [Hz]')
 plt.ylabel('Phase [°]')
-plt.xlim(0, f_s / 2)
+plt.xlim(0, f_s / 2/50)
 plt.ylim(-180, 90)
 plt.yticks([-180, -135, -90, -45, 0, 45, 90])
-plt.axvline(f_c, color='red')
+# plt.axvline(f_marker1, color='blue')
+plt.axvline(f_marker2, color='blue')
+# plt.axvline(f_marker3, color='blue')
+# plt.axvline(f_c, color='red')
+plt.grid()
 plt.show()
-
+''' 
 # Plot the impulse response
 t, y = dimpulse((b, a, 1/f_s), n=2*N)
 plt.suptitle('Impulse Response')
@@ -78,5 +91,6 @@ plt.xlabel('Time [seconds]')
 plt.ylabel('Output')
 plt.xlim(-1/f_s, 2*N/f_s)
 plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1])
-''' 
+
 plt.show()
+''' 

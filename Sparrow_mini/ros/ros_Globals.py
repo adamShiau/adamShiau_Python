6f9d8dcba352e868d845308ebcd3447708857680
adamShiau <ami731025@gmail.com>
2022-04-01 14:32:23 +0800

    Create ros_Globals.py

---
+++ Sparrow_mini/ros/ros_Globals.py
@@ -0,0 +1,99 @@
+## ROS parameters ###
+SF_G_INIT = 9.8
+ORI_X_INIT = -1
+ORI_Y_INIT = 0
+ORI_Z_INIT = 0
+ORI_W_INIT = 0
+COV_ORI_XX_INIT = -1
+COV_ORI_XY_INIT = 0
+COV_ORI_XZ_INIT = 0
+COV_ORI_YX_INIT = 0
+COV_ORI_YY_INIT = 0
+COV_ORI_YZ_INIT = 0
+COV_ORI_ZX_INIT = 0
+COV_ORI_ZY_INIT = 0
+COV_ORI_ZZ_INIT = 0
+COV_W_XX_INIT = 2.44e-3
+COV_W_XY_INIT = 0
+COV_W_XZ_INIT = 0
+COV_W_YX_INIT = 0
+COV_W_YY_INIT = 2.44e-3
+COV_W_YZ_INIT = 0
+COV_W_ZX_INIT = 0
+COV_W_ZY_INIT = 0
+COV_W_ZZ_INIT = 1e-4
+COV_A_XX_INIT = 16.66e-6
+COV_A_XY_INIT = 0
+COV_A_XZ_INIT = 0
+COV_A_YX_INIT = 0
+COV_A_YY_INIT = 14.7e-6
+COV_A_YZ_INIT = 0
+COV_A_ZX_INIT = 0
+COV_A_ZY_INIT = 0
+COV_A_ZZ_INIT = 29.4e-6
+
+global sf_g_init 
+global ori_x_init
+global ori_y_init
+global ori_z_init
+global ori_w_init
+global cov_ori_xx_init
+global cov_ori_xy_init
+global cov_ori_xz_init
+global cov_ori_yx_init
+global cov_ori_yy_init
+global cov_ori_yz_init
+global cov_ori_zx_init
+global cov_ori_zy_init
+global cov_ori_zz_init
+global cov_w_xx_init
+global cov_w_xy_init
+global cov_w_xz_init
+global cov_w_yx_init
+global cov_w_yy_init
+global cov_w_yz_init
+global cov_w_zx_init
+global cov_w_zy_init
+global cov_w_zz_init
+global cov_a_xx_init
+global cov_a_xy_init
+global cov_a_xz_init
+global cov_a_yx_init
+global cov_a_yy_init
+global cov_a_yz_init
+global cov_a_zx_init
+global cov_a_zy_init
+global cov_a_zz_init
+
+sf_g_init 			= SF_G_INIT 
+ori_x_init = ORI_X_INIT
+ori_y_init = ORI_Y_INIT
+ori_z_init = ORI_Z_INIT
+ori_w_init = ORI_W_INIT
+cov_ori_xx_init = COV_ORI_XX_INIT
+cov_ori_xy_init = COV_ORI_XY_INIT
+cov_ori_xz_init = COV_ORI_XZ_INIT
+cov_ori_yx_init = COV_ORI_YX_INIT
+cov_ori_yy_init = COV_ORI_YY_INIT
+cov_ori_yz_init = COV_ORI_YZ_INIT
+cov_ori_zx_init = COV_ORI_ZX_INIT
+cov_ori_zy_init = COV_ORI_ZY_INIT
+cov_ori_zz_init = COV_ORI_ZZ_INIT
+cov_w_xx_init = COV_W_XX_INIT
+cov_w_xy_init = COV_W_XY_INIT
+cov_w_xz_init = COV_W_XZ_INIT
+cov_w_yx_init =  COV_W_YX_INIT
+cov_w_yy_init = COV_W_YY_INIT
+cov_w_yz_init = COV_W_YZ_INIT
+cov_w_zx_init = COV_W_ZX_INIT
+cov_w_zy_init = COV_W_ZY_INIT
+cov_w_zz_init = COV_W_ZZ_INIT
+cov_a_xx_init = COV_A_XX_INIT
+cov_a_xy_init = COV_A_XY_INIT
+cov_a_xz_init = COV_A_XZ_INIT
+cov_a_yx_init = COV_A_YX_INIT
+cov_a_yy_init = COV_A_YY_INIT
+cov_a_yz_init = COV_A_YZ_INIT
+cov_a_zx_init = COV_A_ZX_INIT
+cov_a_zy_init = COV_A_ZY_INIT
+cov_a_zz_init = COV_A_ZZ_INIT

2d396e5d6f8b5e999af8c379f014ddb2896a6c1c
adam1025 <69100634+adamShiau@users.noreply.github.com>
2022-06-14 00:32:55 +0800

    Update np_loaddata.py

--- myAPI/1_readIMU/np_loaddata.py
+++ myAPI/1_readIMU/np_loaddata.py
@@ -3,23 +3,24 @@ import time
 import pandas as pd
 
 NAME = '0613_2' + '.txt'
-# t1 = time.perf_counter()
-# Var = np.loadtxt(NAME, comments='#', delimiter=',', skiprows=0)
-# t2 = time.perf_counter()
-# dt = (t2 - t1) * 1e3
-# shape = np.shape(Var)
-# print(dt)
-# print(shape)
-# print(Var[0])
-# print()
+t1 = time.perf_counter()
+Var = np.loadtxt(NAME, comments='#', delimiter=',', skiprows=0)
+t2 = time.perf_counter()
+dt = (t2 - t1) * 1e3
+shape = np.shape(Var)
+print(dt)
+print(shape)
+# print(Var[:, 0])
+# print(Var[-1])
 
 t1 = time.perf_counter()
 Var = pd.read_csv(NAME, comment='#')
-Var.columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+Var.columns = ['time', 'wx', 'wy', 'wz', 'ax', 'ay', 'az']
 # df = pd.DataFrame(Var, columns=['a', 'b', 'c', 'd', 'e', 'f', 'g'])
 t2 = time.perf_counter()
 dt = (t2 - t1) * 1e3
 
 print(dt)
-print(Var.a)
-print(Var.columns)
+# print(Var)
+print(Var.time[53848])
+# print(len(Var.time))

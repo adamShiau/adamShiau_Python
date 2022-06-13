a63eb723dbc9beed62ba560a48d426db5ae5ee7a
adam1025 <69100634+adamShiau@users.noreply.github.com>
2022-06-13 22:36:26 +0800

    update

--- myAPI/1_readIMU/memsImu_Widget2.py
+++ myAPI/1_readIMU/memsImu_Widget2.py
@@ -13,14 +13,10 @@ from PyQt5.QtWidgets import *
 import sys
 from myLib.myGui.graph import *
 from myLib.myGui.mygui_serial import *
-from myLib.myGui.myLabel import *
-from myLib.myGui.myLineEdit import *
-from myLib.myGui.myCheckBox import *
-from myLib.myGui.myRadioButton import *
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
 
-class memsImuWidget(QWidget):
+class memsImuWidget(QMainWindow):
 
     def __init__(self):
         super(memsImuWidget, self).__init__()
@@ -34,12 +30,13 @@ class memsImuWidget(QWidget):
         self.stop_bt.setEnabled(False)
         self.save_block = dataSaveBlock("save data")
         self.para_block = lineEditBlock('parameter configuration file')
-        self.buffer_lb = displayOneBlock('Buffer Size')
-        self.pd_temp_lb = displayOneBlock('PD Temp.')
-        self.data_rate_lb = displayOneBlock('Data Rate')
-        self.logo_lb = logo('./aegiverse_logo_bk.jpg')
-        self.kal_filter_rb = QRadioButton('Kalman Filter')
+        self.buffer_lb = displayOneBlock('Buffer size')
+        self.pd_temp_lb = displayOneBlock('PD temp.')
+        self.data_rate_lb = displayOneBlock('data rate')
+        self.kal_filter_rb = QRadioButton('Kalman filter')
         self.plot1_showWz_cb = checkBoxBlock_2('Wz', 'pig', 'mems')
+        self.plot1_showWz_cb.cb_1.setChecked(False)
+        self.plot1_showWz_cb.cb_2.setChecked(True)
         self.plot1_unit_rb = radioButtonBlock_2('Unit', 'dph', 'dps')
         self.plot1 = pgGraph_1_2(color1="w", color2="r", title="FOG_WZ VS MEMS_WZ")
         self.plot2 = pgGraph_1(color=(255, 0, 0), title="MEMS_AX [g]")
@@ -50,15 +47,14 @@ class memsImuWidget(QWidget):
 
         layout = QGridLayout()
         layout.addWidget(self.usb.layout(), 0, 0, 1, 4)
-        layout.addWidget(self.read_bt, 0, 5, 1, 1)
+        layout.addWidget(self.start_bt, 0, 5, 1, 1)
         layout.addWidget(self.stop_bt, 0, 6, 1, 1)
         layout.addWidget(self.save_block, 0, 10, 1, 3)
         layout.addWidget(self.para_block, 1, 10, 1, 3)
         layout.addWidget(self.buffer_lb, 1, 4, 1, 1)
         layout.addWidget(self.pd_temp_lb, 1, 5, 1, 1)
         layout.addWidget(self.data_rate_lb, 1, 6, 1, 2)
-        layout.addWidget(self.logo_lb, 0, 13, 2, 7)
-        layout.addWidget(self.kal_filter_rb, 0, 4, 1, 1)
+        layout.addWidget(self.kal_filter_rb, 1, 8, 1, 1)
         layout.addWidget(self.plot1, 2, 0, 10, 10)
         layout.addWidget(self.plot1_showWz_cb, 1, 2, 1, 2)
         layout.addWidget(self.plot1_unit_rb, 1, 0, 1, 2)
@@ -68,6 +64,9 @@ class memsImuWidget(QWidget):
         layout.addWidget(self.plot5, 8, 10, 2, 10)
         layout.addWidget(self.plot6, 10, 10, 2, 10)
 
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
         self.setLayout(layout)
 
     def setBtnEnable(self, en):
@@ -78,7 +77,7 @@ class memsImuWidget(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    w = pigImuWidget()
-
+    w = memsImuWidget()
+    print(w.para_block.le_filename.text())
     w.show()
     app.exec_()

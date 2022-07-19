e23c1cbc23d32eaf59280df1df48191329df4612
adam1025 <69100634+adamShiau@users.noreply.github.com>
2022-07-19 21:49:15 +0800

    Update tt.py

--- myAPI/5_readIMU_GPS/tt.py
+++ myAPI/5_readIMU_GPS/tt.py
@@ -1,7 +1,7 @@
-import numpy as np
+from os.path import exists
 
-A = np.array([1,2,3,4,5])
-B = np.array([1,3,4,4,10])
-D = np.array([0,1,2,3,4])
-C = A+B*5
-print(C)
+file = '07032.txt'
+
+file_exist = exists(file)
+
+print(file_exist)

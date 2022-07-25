2802c93b5b3c7c16c76759985efb826696233041
adamShiau <ami731025@gmail.com>
2022-07-25 18:30:15 +0800

    Update autoSave.py

--- myAPI/myLib/myGui/autoSave.py
+++ myAPI/myLib/myGui/autoSave.py
@@ -1,8 +1,17 @@
 from datetime import datetime
 import time
+from os.path import exists
+import os
 
 
 class atSave_PC:
 
     def __init__(self, rootPath='./', actObj=None):
-        print(datetime.now().second)
+        file_exist = exists(rootPath + 'data')
+        print(file_exist)
+        os.mkdir(rootPath + 'data', mode=0o777)
+        # print(datetime.now().second)
+
+
+if __name__ == "__main__":
+    atSave_PC()

1c8ea1b8f2b63cd32d2663e8950d35faf255c5e5
adamShiau <ami731025@gmail.com>
2022-09-23 15:05:27 +0800

    update

--- myAPI/3_readPigIMU/pigImuReader.py
+++ myAPI/3_readPigIMU/pigImuReader.py
@@ -212,7 +212,7 @@ class pigImuReader(QThread):
     # End of memsImuReader::disconnectIMU
 
     def readIMU(self):
-        self.writeImuCmd(2, 1)
+        self.writeImuCmd(2, 2)
 
     def stopIMU(self):
         self.writeImuCmd(2, 4)

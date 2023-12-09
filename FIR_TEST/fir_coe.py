d412e721e6db894a11c3aaa7c12893cb346669c6
adamShiau <ami731025@gmail.com>
2023-12-10 00:25:07 +0800

    Create fir_coe.py

---
+++ FIR_TEST/fir_coe.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy.signal import firwin
+
+# 截止频率为5MHz，采样频率为10MHz（示例采样频率，根据实际情况调整）
+cutoff_freq = 5e6
+sampling_freq = 100e6
+
+# 设计滤波器
+num_taps = 32
+fir_coeff = firwin(num_taps, cutoff_freq / (sampling_freq / 2), window='hamming')
+
+# 显示滤波器系数
+print("FIR Filter Coefficients:")
+print(*fir_coeff, sep=", ")
